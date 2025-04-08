64245de69f65011eeb8786df33bf2abf2bda488c
vitinpicademel <seu.email@exemplo.com>
2025-04-08 09:34:24 -0300

    Fix: Ajustando layout da seção de Localização com marcadores e descrição

--- src/components/Localizacao.tsx
+++ src/components/Localizacao.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { FaMapMarkerAlt } from 'react-icons/fa';
 import Image from 'next/image';
 
 export default function Localizacao() {
@@ -18,15 +19,39 @@ export default function Localizacao() {
           <Image
             src="/imagens/localizacao.jpg"
             alt="Localização Grand Hill"
-            width={800}
-            height={500}
+            width={1200}
+            height={600}
             className="mapa-img"
           />
+          <div className="mapa-overlay">
+            <div className="marker rifaina">
+              <FaMapMarkerAlt className="marker-icon" />
+              RIFAINA-SP
+            </div>
+            <div className="marker ponte">
+              <FaMapMarkerAlt className="marker-icon" />
+              PONTE
+            </div>
+            <div className="marker grand-hill">
+              <FaMapMarkerAlt className="marker-icon" />
+              GRAND HILL
+            </div>
+          </div>
+          <div className="mapa-description">
+            <p>
+              Localizado às margens da represa Jaguara, próximo à ponte do Rio Grande,
+              que faz divisa entre os estados de São Paulo e Minas Gerais, está inserido
+              na área de turismo e lazer, e possui vista para o município de Rifaina-SP.
+            </p>
+          </div>
         </div>
 
         <div className="endereco-card">
           <h3>Endereço</h3>
-          <p>Rifaina - SP</p>
+          <p>
+            <FaMapMarkerAlt className="endereco-icon" />
+            Rifaina - SP
+          </p>
           <p>
             Localizado às margens da represa Jaguara, próximo à ponte do Rio Grande,
             que faz divisa entre os estados de São Paulo e Minas Gerais, está inserido
